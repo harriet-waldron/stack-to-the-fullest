e908e2a2f8bf86a5615bad040bd7dfff96ca5c35
Harriet Waldron <harriet_waldron@hotmail.com>
2021-07-05 18:50:24 +1200

    setup

--- server/routes/planets.js
+++ server/routes/planets.js
@@ -1,18 +1,18 @@
 const express = require('express')
 
-const db = require('../db/fruits')
+const db = require('../db/planets')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  db.getFruits()
+  db.getPlanets()
     .then(results => {
-      res.json({ fruits: results.map(fruit => fruit.name) })
+      res.json({ planets: results.map(planet => planet.name) })
       return null
     })
     .catch(err => {
       console.log(err)
-      res.status(500).json({ message: 'Somthing went wrong' })
+      res.status(500).json({ message: 'Something went wrong' })
     })
 })
 

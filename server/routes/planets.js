204932f3039e2efbb63cfdedd64cb987eec21d60
Harriet Waldron <harriet_waldron@hotmail.com>
2021-09-06 18:10:16 +1200

    take away d3 practice to move to another repo

--- server/routes/planets.js
+++ server/routes/planets.js
@@ -1,19 +1,19 @@
-const express = require('express')
+// const express = require('express')
 
-const db = require('../db/planets')
+// const db = require('../db/planets')
 
-const router = express.Router()
+// const router = express.Router()
 
-router.get('/', (req, res) => {
-  db.getPlanets()
-    .then(results => {
-      res.json({ planets: results.map(planet => planet.name) })
-      return null
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({ message: 'Something went wrong' })
-    })
-})
+// router.get('/', (req, res) => {
+//   db.getPlanets()
+//     .then(results => {
+//       res.json({ planets: results.map(planet => planet.name) })
+//       return null
+//     })
+//     .catch(err => {
+//       console.log(err)
+//       res.status(500).json({ message: 'Something went wrong' })
+//     })
+// })
 
-module.exports = router
+// module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const path = require('path')
 
-const planetRoutes = require('./routes/planets')
+// const planetRoutes = require('./routes/planets')
 const maoriSkyRoutes = require('./routes/maorisky')
-const authRoutes = require('./routes/auth')
-const userStoryRoutes = require('./routes/userstory')
+// const authRoutes = require('./routes/auth')
+// const userStoryRoutes = require('./routes/userstory')
 
 
 const server = express()
@@ -12,10 +12,10 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/planets', planetRoutes)
+// server.use('/api/v1/planets', planetRoutes)
 server.use('/api/v1/maorisky', maoriSkyRoutes)
-server.use('/api/v1', authRoutes)
-server.use('/api/v1/userstories', userStoryRoutes)
+// server.use('/api/v1', authRoutes)
+// server.use('/api/v1/userstories', userStoryRoutes)
 server.get('/', (req, res) => {
   res.send()
 })
